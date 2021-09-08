d11fa6c44452bfa18a2b36cc34c24d31f7e8a2d3
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-09-07 20:05:00 -0400

    Update Fibonacci2.java

--- Homework_1/Fibonacci2.java
+++ Homework_1/Fibonacci2.java
@@ -5,17 +5,28 @@
     Description: 
  */
 import java.util.Scanner;
-import java.time.format.*;
+//import java.time.format.*;
+import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
 public class Fibonacci2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		int num = 0;
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Fibonacci Numbers, input a number:");
 		num = scan.nextInt();
+	    long startTime = System.nanoTime();
 		
 		System.out.println("Fibonacci of " + num + " is " + fib(num));	
+	 
+	    long endTime = System.nanoTime();
+	 
+	    long lengthInNano = (endTime - startTime); 
+	     
+	    long lengthInMillis = TimeUnit.NANOSECONDS.toMillis(lengthInNano);  //Total execution time in mills seconds
+	    System.out.println("Length in nano-seconds " + lengthInNano);
+	    System.out.println("Length in milli-seconds " + lengthInMillis);
 	}
 	
 	public static int fib(int num) {
