3e99e8b1f6b7ef9899ba12ec02629589dee40555
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-10-27 20:40:16 -0400

    Update Infix_And_Postfix.java

--- Lab_4/Infix_And_Postfix.java
+++ Lab_4/Infix_And_Postfix.java
@@ -1,38 +1,84 @@
+/*
+    Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+    Date:
+    Instructor: Professor Silveyra
+    Description: Develop and Implement a Program in Java for converting an Infix Expression to a Postfix Expression
+    and vice versa.
+ */
 public class Infix_And_Postfix<T> implements StackInterface<T> {
-    T expression;
-    T a;
-    T b;
-    T c;
+    private Node head;
+    public class Node {
+        private T node_value;
+        Node next;
 
-    public Infix_And_Postfix(T expression) {
-//        this.expression = expression;
+        public Node(T obj) {
+            node_value = obj;
+            next = null;
+        }
+        public T value() {
+            return node_value;
+        }
+    }
+//    Constructor
+    public Infix_And_Postfix() {
+        Node head = new Node(null);
     }
 
-    public void push(T number) {
+    public void push(T operation) {
+        Node element = new Node(operation);
+//
+//        if (isEmpty()) {
+//            head.next = element;
+//        }
+//        else {
+//            element.next = head.next;
+//            head.next = element;
+//        }
+        element.next = head;         // New node points to current front item.
 
-    }
-    public void push(String number) {
+        head = element;              // The new node becomes the front node.
     }
 
     public T pop() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        else {
+//            Node element = head.next;
+//            head.next = element.next;
+//            element.next = null;
+//            return element.value();
+            Node element = head;
+            head = head.next;
+            element.next = null;
+            return element.node_value;
+        }
     }
 
     public T peek() {
-        return null;
+        return head.next.value();
     }
 
     public boolean isEmpty() {
-        System.out.println("hey");
-        return true;
+        if (head == null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void printOutPut() {
+        Node traverse = head;
+
+        while (traverse != null) {
+            System.out.println(traverse.node_value);
+            traverse = traverse.next;
+        }
     }
 
     public boolean isFull() {
         return false;
     }
 
-    public static void main(String[] args) {
-//        Infix_And_Postfix userExpression = new Infix_And_Postfix("12+")
-
-    }
 }
