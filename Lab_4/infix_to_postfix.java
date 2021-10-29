9373aa7fbbb294fd7c4a2169af3e235c158f9383
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-10-28 20:02:58 -0400

    Update infix_to_postfix.java

--- Lab_4/infix_to_postfix.java
+++ Lab_4/infix_to_postfix.java
@@ -1,4 +1,3 @@
-
 /*
     Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
     Date:
@@ -6,36 +5,74 @@
     Description: Develop and Implement a Program in Java for converting an Infix Expression to a Postfix Expression
     and vice versa.
  */
-public class postfix_to_infix {
+public class infix_to_postfix {
     Infix_And_Postfix<String> operationsStack;
 
-    public postfix_to_infix() {
+    public infix_to_postfix() {
         this.operationsStack = new Infix_And_Postfix<String>();
     }
 
     public String conversion (String val) {
-        String a = "";
-        String b = "";
-        String c = "";
-        String x = "";
+        String finalOutput = "";
+
+        StringBuilder expression = new StringBuilder();
 
         for (char letterChar: val.toCharArray()) {
             String currentRule = Character.toString(letterChar);
-            if (currentRule.matches("-?(0|[1-9]\\d*)")) {
-                operationsStack.push(currentRule);
+            if ( currentRule.matches("-?(0|[1-9]\\d*)") ) {
+                expression.append(currentRule);
             }
+            else {
+//                1st case
+                if (operationsStack.peek() == null) {
+                    operationsStack.push(currentRule);
+                }
+                else if (currentRule.equals("(")) {
+                    operationsStack.push(currentRule);
+                }
+                else if (currentRule.equals(")")) {
+                    while (!operationsStack.peek().equals("(")) {
+                        expression.append(operationsStack.pop());
+                    }
+                    operationsStack.pop();
+                }
 
-            else if (currentRule.equals("+") || currentRule.equals("-") || currentRule.equals("*")
-                    || currentRule.equals("/") || currentRule.equals("%")) {
-                a = operationsStack.pop();
-                c = operationsStack.pop();
-                b = currentRule;
+                else if (currentRule.equals("*") || currentRule.equals("/") || currentRule.equals("%")) {
+                    if (operationsStack.peek() == null) {
+                        operationsStack.push(currentRule);
+                    }
+                    else if (operationsStack.peek().equals("*") || operationsStack.peek().equals("/") || operationsStack.peek().equals("%")) {
+                        while (!operationsStack.isEmpty() && !operationsStack.peek().equals("(")) {
+                            expression.append(operationsStack.pop());
+                        }
+                        operationsStack.push(currentRule);
+                    }
 
-                x = c + b + a;
+                    else if (operationsStack.peek().equals("+") || operationsStack.peek().equals("-"))  {
+                        operationsStack.push(currentRule);
+                    }
 
-                operationsStack.push(x);
+                }
+
+                else if (currentRule.equals("+") || currentRule.equals("-")) {
+                    if (operationsStack.peek() == null) {
+                        operationsStack.push(currentRule);
+                    }
+                    else if (operationsStack.peek().equals("*") || operationsStack.peek().equals("/") || operationsStack.peek().equals("+") || operationsStack.peek().equals("-") || operationsStack.peek().equals("%")) {
+                        while (!operationsStack.isEmpty() && !operationsStack.peek().equals("(")) {
+                            expression.append(operationsStack.pop());
+                        }
+                        operationsStack.push(currentRule);
+                    }
+                }
             }
         }
-        return operationsStack.pop();
+
+        while (!operationsStack.isEmpty()) {
+            expression.append(operationsStack.pop());
+        }
+
+       finalOutput = expression.toString();
+        return finalOutput;
     }
 }
