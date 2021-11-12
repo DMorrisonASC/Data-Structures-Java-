d6397cba27714dab53281d71c32305455040e710
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-11-12 16:18:01 -0500

    Update BTS.java

--- Homework_4/BTS.java
+++ Homework_4/BTS.java
@@ -6,9 +6,6 @@
     Write a function to find the depth of a given node and the depth of the tree. Write the function SwapTree(). This
     function swaps the left and the right children of every node.
  */
-
-import java.util.Random;
-
 // The following code is the heavily modified version of code provided by
 public class BTS<T>{
 
@@ -114,12 +111,12 @@ public class BTS<T>{
     if (compareTo(input) == 0) {
       inputNum = Integer.parseInt(input.toString());
       if (x == null) {
-        System.out.println("There is NO node value of \"" + x.value + "\" present in the tree.");
+        System.out.println("There is NO node value present in the tree.");
         return null;
       }
-      if (Integer.parseInt(x.value.toString()) < inputNum) {
+      if (inputNum < Integer.parseInt(x.value.toString()) ) {
         x.left = findNode_recur(x.left, input);
-      } else if (Integer.parseInt(x.value.toString()) > inputNum) {
+      } else if (inputNum > Integer.parseInt(x.value.toString())) {
         x.right = findNode_recur(x.right, input);
       } else {
         System.out.println("There is a node value of \"" + x.value + "\" present in the tree.");
@@ -130,12 +127,12 @@ public class BTS<T>{
     else if (compareTo(input) == 2) {
       intDouble = Double.parseDouble(input.toString());
       if (x == null) {
-        System.out.println("There is NO node value of \"" + x.value + "\" present in the tree.");
+        System.out.println("There is NO node value present in the tree.");
         return null;
       }
-      if (Double.parseDouble(x.value.toString()) < intDouble) {
+      if (intDouble < Double.parseDouble(x.value.toString())) {
         x.left = findNode_recur(x.left, input);
-      } else if (Double.parseDouble(x.value.toString()) > intDouble) {
+      } else if (intDouble > Double.parseDouble(x.value.toString())) {
         x.right = findNode_recur(x.right, input);
       } else {
         System.out.println("There is a node value of \"" + x.value + "\" present in the tree.");
@@ -146,12 +143,12 @@ public class BTS<T>{
     else {
       inputChar = input.toString().charAt(0);
       if (x == null) {
-        System.out.println("There is NO node value of \"" + x.value + "\" present in the tree.");
+        System.out.println("There is NO node value present in the tree.");
         return null;
       }
-      if (x.value.toString().charAt(0) < inputChar) {
+      if (inputChar < x.value.toString().charAt(0)) {
         x.left = findNode_recur(x.left, input);
-      } else if (x.value.toString().charAt(0) > inputChar) {
+      } else if (inputChar > x.value.toString().charAt(0)) {
         x.right = findNode_recur(x.right, input);
       } else {
         System.out.println("There is a node value of \"" + x.value + "\" present in the tree.");
@@ -221,16 +218,12 @@ public class BTS<T>{
         node.right = deleteNode_recur(node.right, value);
       } else {
         if (node.left == null && node.right == null) {
-          System.out.println("It had no child elements");
           node = null;
         } else if (node.left == null) {
-          System.out.println("Switched places with right child");
           node = node.right;
         } else if (node.right == null) {
-          System.out.println("Switched places with left child");
           node = node.left;
         } else {
-          System.out.println("case");
           Node successor = findSuccessor_recur(node.right);
 //
           node.value = successor.value;
@@ -267,7 +260,6 @@ public class BTS<T>{
           node.value = successor.value;
 //
           node.right = deleteNode_recur(node.right, successor.value);
-
         }
       }
       return node;
@@ -282,6 +274,65 @@ public class BTS<T>{
       return findSuccessor_recur(node.left);
     }
   }
+
+  public void findDepth(T input) {
+    root = findDepth_recur(root, input, 0);
+  }
+
+  private Node findDepth_recur(Node x, T input, int depth) {
+    int inputNum;
+    double intDouble;
+    char inputChar;
+    if (compareTo(input) == 0) {
+      inputNum = Integer.parseInt(input.toString());
+      if (x == null) {
+        System.out.println("There is NO node value present in the tree.");
+        return null;
+      }
+      if (inputNum < Integer.parseInt(x.value.toString()) ) {
+        x.left = findDepth_recur(x.left, input, depth + 1);
+      } else if (inputNum > Integer.parseInt(x.value.toString())) {
+        x.right = findDepth_recur(x.right, input, depth + 1);
+      } else {
+        System.out.println("This node has a depth of " + depth);
+      }
+      return x;
+    }
+
+    else if (compareTo(input) == 2) {
+      intDouble = Double.parseDouble(input.toString());
+      if (x == null) {
+        System.out.println("There is NO node value present in the tree.");
+        return null;
+      }
+      if (intDouble < Double.parseDouble(x.value.toString())) {
+        x.left = findDepth_recur(x.left, input, depth + 1);
+      } else if (intDouble > Double.parseDouble(x.value.toString())) {
+        x.right = findDepth_recur(x.right, input, depth + 1);
+      } else {
+        System.out.println("This node has a depth of " + depth);
+      }
+      return x;
+    }
+
+    else {
+      inputChar = input.toString().charAt(0);
+      if (x == null) {
+        System.out.println("There is NO node value present in the tree.");
+        return null;
+      }
+      if (inputChar < x.value.toString().charAt(0) ) {
+        x.left = findDepth_recur(x.left, input, depth + 1);
+      } else if (inputChar > x.value.toString().charAt(0)) {
+        x.right = findDepth_recur(x.right, input, depth + 1);
+      } else {
+        System.out.println("This node has a depth of " + depth);
+      }
+      return x;
+    }
+  }
+
+
 
   public void preorder(){
     preorderRecursive(root);
@@ -325,7 +376,13 @@ public class BTS<T>{
     System.out.println("Post-order:");
     postorder();
   }
-
+  /**
+   * Returns an int that can be used in generic methods to identify if a value is a int, double or char.
+   *
+   * @param  a  a generic that looks for primitive types.
+   * @return  An int that 1 or 2 or 3. Return 1 of the input of "a" is an integer. Return 2 of the input of "a" is an double.
+   * Return 3 of the input of "a" is an string.
+   */
   public int compareTo(T a) {
     String temp1 = a.toString();
 //    Return "0" if it's an integer
