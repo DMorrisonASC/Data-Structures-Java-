b1f9e378dc8c2fb01f8cde65ba3e1aa18308631d
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-11-12 11:43:29 -0500

    Update Menu.java

--- Homework_4/Menu.java
+++ Homework_4/Menu.java
@@ -1,6 +1,3 @@
-import java.util.Locale;
-import java.util.Random;
-import java.util.Scanner;
 /*
     Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
     Date:
@@ -8,15 +5,20 @@ import java.util.Scanner;
     Description: Develop and Implement a Program in Java for converting an Infix Expression to a Postfix Expression
     and vice versa.
  */
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
 public class Menu {
+    /*
+    * When run, it creates 3 binary trees of type Integer, Double or Character. It shows a menu that allows user to pick a tree they want
+    * to modify: Delete, insert, find or print reorder, inorder, and postorder visualization of the Tree. They can also choose to exit.
+    * */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         BTS<Integer> treeInt = new BTS<Integer>();
         BTS<Double> treeDouble = new BTS<Double>();
         BTS<Character> treeChar = new BTS<Character>();
         Random rand = new Random();
-
-
         System.out.println("Welcome to Infix & Postfix converter: \n----------------\n");
 
         boolean keepGoing = true;
@@ -89,29 +91,47 @@ public class Menu {
                     }
                     break;
                 case "3":
-                    System.out.println("What element to delete?:");
-                    int userInput = userExpression.nextInt();
-//                    treeInt.delete(userInput);
+                    System.out.println("What element to delete?: 1) Integers or 2)Doubles or 3) Character values?");
+                    String typeOfInput4 = keyboard.next();
+                    switch (typeOfInput4) {
+                        case "1":
+                            System.out.println("Enter integer: ");
+                            int intToFind = userExpression.nextInt();
+                            treeInt.delete(intToFind);
+                            break;
+                        case "2":
+                            System.out.println("Enter double: ");
+                            double doubleToFind = userExpression.nextDouble();
+                            treeDouble.delete(doubleToFind);
+                            break;
+                        case "3":
+                            System.out.println("Enter character: ");
+                            char charToFind = userExpression.next().toUpperCase(Locale.ROOT).trim().charAt(0);
+                            treeChar.delete(charToFind);
+                            break;
+                        default:
+                            System.out.println("That's not an answer");
+                    }
                     break;
                 case "4":
                     System.out.println("What element to find?: 1) Integers or 2)Doubles or 3) Character values?");
                     String typeOfInput3 = keyboard.next();
                     switch (typeOfInput3) {
-                        case "1":
+                        case "1" -> {
                             System.out.println("Enter integer: ");
                             int intToFind = userExpression.nextInt();
                             treeInt.findNode(intToFind);
-                            break;
-                        case "2":
+                        }
+                        case "2" -> {
                             System.out.println("Enter double: ");
                             double doubleToFind = userExpression.nextDouble();
                             treeDouble.findNode(doubleToFind);
-                            break;
-                        case "3":
+                        }
+                        case "3" -> {
                             System.out.println("Enter character: ");
-                            char charToFind = userExpression.next().toUpperCase(Locale.ROOT).charAt(0);
+                            char charToFind = userExpression.next().toUpperCase(Locale.ROOT).trim().charAt(0);
                             treeChar.findNode(charToFind);
-                            break;
+                        }
                     }
 
                     break;
