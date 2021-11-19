6515d0584d96ee9abf1bc9cf29bbe28b896b0ccc
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-11-18 22:38:47 -0500

    Add files via upload

---
+++ Lab_5/Menu.java
@@ -0,0 +1,21 @@
+/*
+    Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+    Date:
+    Instructor: Professor
+    Description: A menu that displays data from an open hash table and a heap.
+ */
+
+public class Menu {
+
+    public static void main(String[] args) {
+        boolean keepGoing = true;
+
+        while (keepGoing) {
+            System.out.println("Which data structure?: ");
+            System.out.println("\t1. Hash");
+            System.out.println("\t2. Heap");
+            System.out.println("\t3. Hash");
+
+        }
+    }
+}
