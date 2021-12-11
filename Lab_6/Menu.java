ac83cc2d5710d5017aac5c25f77ca1f9f8324a6b
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-12-11 15:50:40 -0500

    Update Menu.java

--- Lab_6/Menu.java
+++ Lab_6/Menu.java
@@ -5,17 +5,35 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) throws IOException {
         File fileName = new File("./Example_Data.txt");
-
         Scanner myFile = new Scanner(fileName);
+        Adjacency_List myList;
+        int adjListSize;
+        String amountOfEdges;
 
+        // Scan in the 1st line in file that sets size of adjacency list and it's edges
+        String theFirstLine = myFile.nextLine();
+        Scanner theFirstLineScanner = new Scanner(theFirstLine);
+
+        adjListSize = Integer.parseInt(theFirstLineScanner.next());
+        amountOfEdges = theFirstLineScanner.next();
+        myList = new Adjacency_List(adjListSize);
+
+        /*
+        * Read and load in data from example graph
+        * */
         while (myFile.hasNextLine()) {
+//            Scan in one line in file
             String eachLine = myFile.nextLine();
             Scanner scanNum = new Scanner(eachLine);
-
+//          Scan number
             while (scanNum.hasNext()) {
-                String eachNum = scanNum.next();
-                System.out.println(eachNum);
+                int index = Integer.parseInt(scanNum.next());
+                String to_node_name = scanNum.next();
+                int weight = Integer.parseInt(scanNum.next());
+
+                myList.insert(index, to_node_name, weight);
             }
         }
+        myList.displayAllList();
     }
 }
