6f4fcfed2d7970ab28962dee499a99a36dcc7e03
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-12-12 22:50:13 -0500

    Update Menu.java

--- Lab_6/Menu.java
+++ Lab_6/Menu.java
@@ -5,37 +5,8 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) throws IOException {
         boolean keepGoing = true;
-        File fileName = new File("./Example_Data.txt");
-        Scanner myFile = new Scanner(fileName);
-        Adjacency_List myList;
-        int adjListSize;
-        int amountOfEdges;
-
-        // Scan in the 1st line in file that sets size of adjacency list and it's edges
-        String theFirstLine = myFile.nextLine();
-        Scanner theFirstLineScanner = new Scanner(theFirstLine);
-
-        adjListSize = Integer.parseInt(theFirstLineScanner.next());
-        amountOfEdges = Integer.parseInt(theFirstLineScanner.next());
-        myList = new Adjacency_List(adjListSize, amountOfEdges);
-
-        /*
-        * Read and load in data from example graph
-        * */
-        while (myFile.hasNextLine()) {
-//            Scan in one line in file
-            String eachLine = myFile.nextLine();
-            Scanner scanNum = new Scanner(eachLine);
-//          Scan number
-            while (scanNum.hasNext()) {
-                int index = Integer.parseInt(scanNum.next());
-                String to_node_name = scanNum.next();
-                int weight = Integer.parseInt(scanNum.next());
-
-                myList.insert(index, to_node_name, weight);
-            }
-        }
-//        myList.displayAllList();
+        Adjacency_List myList = new Adjacency_List();
+        myList = myList.load_List_File("./Example_Data.txt");
 
         Scanner keyboard = new Scanner(System.in);
 
@@ -59,16 +30,14 @@ public class Menu {
                 break;
 
                 case "3":
-
+                    System.out.println("Enter filename: ");
+                    String load_FileName = keyboard.nextLine().trim();
+                    myList = myList.load_List_File(load_FileName);
                     break;
                 case "4":
                     keepGoing = false;
                 break;
-
             }
-
         }
-
     }
-    
 }
